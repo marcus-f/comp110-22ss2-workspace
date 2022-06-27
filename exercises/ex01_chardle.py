e39a4c8633219c6d59861b9140b1b1377f02b0c4
Marcus Fewell <mfewell@unc.edu>
2022-06-26 22:15:07 -0400

    corrected final

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -7,35 +7,35 @@ if len(secret_word) != 5:
     print("Error: Word must contain 5 characters")  
     exit()
 
-guess_letter: str = input("Enter a single character: ")
-if len(guess_letter) != 1:  
-    print("Error: Character must be a singular character.") 
+guess_letter: str = input("Enter a single character: ") 
+if len(guess_letter) != 1: 
+    print("Error: Character must be a singular character.")  
     exit()
 
 times: int = 0  
-print("Searching for " + guess_letter + " in "+ secret_word ) 
+print("Searching for " + guess_letter + " in " + secret_word) 
 
-if(secret_word[0]==guess_letter): 
+if (secret_word[0] == guess_letter):  
     times = times + 1
-    print(guess_letter+ " found at index 0 ")
-if(secret_word[1]==guess_letter):
+    print(guess_letter + " found at index 0 ")
+if (secret_word[1] == guess_letter):
     times = times + 1
-    print(guess_letter+ " found at index 1 ")
-if(secret_word[2]==guess_letter):
+    print(guess_letter + " found at index 1 ")
+if (secret_word[2] == guess_letter):
     times = times + 1
-    print(guess_letter+ " found at index 2 ")
-if(secret_word[3]==guess_letter):
+    print(guess_letter + " found at index 2 ")
+if (secret_word[3] == guess_letter):
     times = times + 1
-    print(guess_letter+ " found at index 3 ")
-if(secret_word[4]==guess_letter):
+    print(guess_letter + " found at index 3 ")
+if (secret_word[4] == guess_letter):
     times = times + 1
-    print(guess_letter+ " found at index 4 ")
+    print(guess_letter + " found at index 4 ")
 
 
 if times == 0:
-    print("No instances of " + guess_letter + " found in  " + secret_word)
+    print("No instances of " + guess_letter + " found in " + secret_word)
 else:
     if times == 1:
-        print(str(times)+ " instances of " + guess_letter + " found in " + secret_word)
+        print(str(times) + " instance of " + guess_letter + " found in " + secret_word)
     else:
-        print(str(times)+ " instance of " + guess_letter + " found in " + secret_word)
+        print(str(times) + " instances of " + guess_letter + " found in " + secret_word)
